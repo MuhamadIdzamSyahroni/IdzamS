10abebb67992bbc85b5e04eb57617fd4cea53711
MuhamadIdzamSyahroni <92902353+MuhamadIdzamSyahroni@users.noreply.github.com>
2021-10-27 22:12:22 +0700

    IdzamS
    Aplikasi Kotlin

---
+++ app/src/main/java/com/example/aplikasikotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.aplikasikotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun submit_btn(view: View) {
+        val namaEditText = findViewById<EditText>(R.id.nama_edit_text)
+        val nama = namaEditText.text.toString()
+
+        val toast = Toast.makeText(applicationContext, nama, LENGTH_LONG)
+        toast.show()
+    }
+}
